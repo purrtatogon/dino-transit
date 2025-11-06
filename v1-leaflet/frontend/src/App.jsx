fa565051d6d698e04d0a5b99bd49a314041c7aa2
s0raia <53994316+s0raia@users.noreply.github.com>
2025-11-06 16:24:37 +0000

    Set up React frontend with Vite

---
+++ v1-leaflet/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppShell from './components/Map/AppShell';
+import styles from '@styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.root}>
+      <AppShell />
+    </div>
+  );
+}
+
+export default App;
